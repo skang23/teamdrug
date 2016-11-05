1eaab827400039b3ebedf20f57ff8087c4cd1507
Suyeon Kang <skang23@wustl.edu>
2016-11-05 14:18:49 -0500

    nearbystore

--- MEDMEN/src/pages/near-by-store/near-by-store.ts
+++ MEDMEN/src/pages/near-by-store/near-by-store.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { AuthData } from '../../providers/auth-data';
@@ -10,23 +10,201 @@ import { LoginPage } from '../login/login';
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
+declare var google;
 @Component({
   selector: 'page-near-by-store',
   templateUrl: 'near-by-store.html'
 })
 export class NearByStorePage {
 
-  constructor(public navCtrl: NavController, public authData: AuthData) {}
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  lat: any;
+  lng: any;
+  constructor(public navCtrl: NavController, public authData: AuthData) { }
 
   ionViewDidLoad() {
-    console.log('Hello NearByStorePage Page + abc');
-    console.log('hello hello');
-    console.log('hello');
+    console.log('Hello NearByStorePage Page');
+    var Map;
+    var Infowindow;
+    var Latitude = undefined;
+    var Longitude = undefined;
+
+    // Get geo coordinates
+
+    function getPlacesLocation() {
+      navigator.geolocation.getCurrentPosition
+        (onPlacesSuccess, onPlacesError, { enableHighAccuracy: true });
+    }
+
+    // Success callback for get geo coordinates
+
+    var onPlacesSuccess = function (position) {
+
+      Latitude = position.coords.latitude;
+      Longitude = position.coords.longitude;
+
+
+      getPlaces(Latitude, Longitude);
+
+    }
+
+    // Get places by using coordinates
+
+    function getPlaces(latitude, longitude) {
+
+      var latLong = new google.maps.LatLng(latitude, longitude);
+
+      var mapOptions = {
+
+        center: new google.maps.LatLng(latitude, longitude),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+
+      };
+
+      
+      Map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      console.log(Map);      
+
+      Infowindow = new google.maps.InfoWindow();
+
+      var service = new google.maps.places.PlacesService(Map); 
+      
+      service.nearbySearch({
+        location: latLong,
+        radius: 500,
+        type: ['pharmacy']
+      }, foundStoresCallback);
+
+    }
+
+    // Success callback for watching your changing position
+
+    var onPlacesWatchSuccess = function (position) {
+
+      var updatedLatitude = 40.758; 
+      //position.coords.latitude;
+      var updatedLongitude = -73.99 
+      //position.coords.longitude;
+
+      if (updatedLatitude != Latitude && updatedLongitude != Longitude) {
+
+        Latitude = updatedLatitude;
+        Longitude = updatedLongitude;
+
+        getPlaces(updatedLatitude, updatedLongitude);
+        
+      }
+    }
+
+    // Success callback for locating stores in the area
+
+    function foundStoresCallback(results, status) {
+
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+
+        for (var i = 0; i < results.length; i++) {
+
+          createMarker(results[i]);
+
+        }
+      }
+    }
+    function getDetailsCallback(results, status) {
+
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+
+        for (var i = 0; i < results.length; i++) {
+
+          createMarker(results[i]);
+
+        }
+      }
+    }
+
+    // Place a pin for each store on the map
+
+    function createMarker(place) {
+
+      var placeLoc = place.geometry.location;
+
+      var marker = new google.maps.Marker({
+        map: Map,
+        position: place.geometry.location
+      });
+
+      google.maps.event.addListener(marker, 'click', function () {
+        console.log(place);
+        Infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+                'Place ID: ' + place.place_id + '<br>' +
+                place.vicinity + '</div>');
+        Infowindow.open(Map, this);
+
+      });
+    }
+
+    // Error callback
+
+    function onPlacesError(error) {
+      console.log('code: ' + error.code + '\n' +
+        'message: ' + error.message + '\n');
+    }
+
+    // Watch your changing position
+
+    function watchPlacesPosition() {
+
+      return navigator.geolocation.watchPosition
+        (onPlacesWatchSuccess, onPlacesError, { enableHighAccuracy: true });
+    }
+    watchPlacesPosition();
+    console.log('Hello NearByStorePage Page end');
+
+
+  }
+
+  loadMap() {
+
+    let latLng = new google.maps.LatLng(this.lat, this.lng);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+  }
+  addMarker() {
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+
+    let content = "<h4>Information!</h4>";
+
+    this.addInfoWindow(marker, content);
+
+  }
+  addInfoWindow(marker, content) {
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+
   }
   logOut() {
-  	this.authData.logoutUser().then(()=>{
-  		this.navCtrl.setRoot(LoginPage);
-  	});
+    this.authData.logoutUser().then(() => {
+      this.navCtrl.setRoot(LoginPage);
+    });
   }
 
 }
