30360f123fe7f383ab46f70b5f9e8fbd61ca528d
Suyeon Kang <skang23@wustl.edu>
2016-11-05 20:58:21 -0500

    profile

--- MEDMEN/src/pages/profile/profile.ts
+++ MEDMEN/src/pages/profile/profile.ts
@@ -1,22 +1,106 @@
+import { NavController, AlertController } from 'ionic-angular';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ProfileData } from '../../providers/profile-data';
+import { AuthData } from '../../providers/auth-data';
+import { LoginPage } from '../login/login';
 
-/*
-  Generated class for the Profile page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-profile',
-  templateUrl: 'profile.html'
+  templateUrl: 'profile.html',
 })
 export class ProfilePage {
+  public userProfile: any;
+  public birthDate: string;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public nav: NavController, public profileData: ProfileData,
+    public authData: AuthData, public alertCtrl: AlertController) {
+    this.nav = nav;
+    this.profileData = profileData;
 
-  ionViewDidLoad() {
-    console.log('Hello ProfilePage Page');
+    this.profileData.getUserProfile().on('value', (data) => {
+      this.userProfile = data.val();
+      this.birthDate = this.userProfile.birthDate;
+    });
+
   }
+  updateName(){
+  let alert = this.alertCtrl.create({
+    message: "Your first name & last name",
+    inputs: [
+      {
+        name: 'firstName',
+        placeholder: 'Your first name',
+        value: this.userProfile.firstName
+      },
+      {
+        name: 'lastName',
+        placeholder: 'Your last name',
+        value: this.userProfile.lastName
+      },
+    ],
+    buttons: [
+      {
+        text: 'Cancel',
+      },
+      {
+        text: 'Save',
+        handler: data => {
+          this.profileData.updateName(data.firstName, data.lastName);
+        }
+      }
+    ]
+  });
+  alert.present();
+}
+updateDOB(birthDate){
+  this.profileData.updateDOB(birthDate);
+}
+updateEmail(){
+  let alert = this.alertCtrl.create({
+    inputs: [
+      {
+        name: 'newEmail',
+        placeholder: 'Your new email',
+      },
+    ],
+    buttons: [
+      {
+        text: 'Cancel',
+      },
+      {
+        text: 'Save',
+        handler: data => {
+          this.profileData.updateEmail(data.newEmail);
+        }
+      }
+    ]
+  });
+  alert.present();
+}
+
+updatePassword(){
+  let alert = this.alertCtrl.create({
+    inputs: [
+      {
+        name: 'newPassword',
+        placeholder: 'Your new password',
+        type: 'password'
+      },
+    ],
+    buttons: [
+      {
+        text: 'Cancel',
+      },
+      {
+        text: 'Save',
+        handler: data => {
+          this.profileData.updatePassword(data.newPassword);
+          
+        }
+      }
+    ]
+  });
+  alert.present();
+}
 
 }
